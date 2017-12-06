3b052aeeb0602849df0036d2fe415e0d7e7660a2
sflagl <samanthaflagler@gmail.com>
2017-12-06 18:33:52 -0500

    testing to make sure if the users selects the correct words the alert you win comes up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,22 @@
 // creat an array of words
 
-dancersToGuess = ['Micheal Jackson', 'Usher', 'Justin Timberlake', 'Ciara'];
+dancersToGuess = ['jackson', 'usher', 'timberlake', 'ciara'];
 
 // Choose word randomly
 
 	var arrNum = Math.floor(Math.random() * dancersToGuess.length);
-	var dancerSelection = dancersToGuess[arrNum]
+	var dancerSelection = dancersToGuess[arrNum];
+	// if right push to right array
+	var rightArray = [];
+	// if wrong push to wrong array
+	var wrongArray = [];
+	var dancerHolder = [];
+	
 	console.log(dancerSelection);
 
 // Create placeholders based on word length
 
-	var dancerHolder = [];
+	
 	for (var i = 0; i < dancerSelection.length; i++) {
 		var createPlaceholder = dancerHolder.push('_');
 
@@ -22,19 +28,35 @@ dancersToGuess = ['Micheal Jackson', 'Usher', 'Justin Timberlake', 'Ciara'];
 	document.addEventListener("keypress", function(event) {
 		// var changeToCode = event.keyCode;
 		var changeToLetter = String.fromCharCode(event.keyCode);
-		console.log(changeToLetter);
-		
+		// console.log(dancerSelection.indexOf(changeToLetter));
+		// if users guess is right
+		if(dancerSelection.indexOf(changeToLetter) > -1){
+			// console.log(true);
+			rightArray.push(changeToLetter);
+			// console.log(rightArray);
+			dancerHolder[dancerSelection.indexOf(changeToLetter)] = changeToLetter;
+			// console.log(changeToLetter);
 
-});
+			if(dancerHolder.join('') == dancerSelection) {
+				alert('you win!');
+			}
+		} 
+
+		else{
+			wrongArray.push(changeToLetter);
+			// console.log(wrongArray);
+		}
+
+	});
 
 	
 		
 // check if guess is right
 
-// if right push to right array
 
-	wrongArray = [];
 
-// if wrong push to wrong array
 	
-	rightArray = [];
+
+
+	
+	
