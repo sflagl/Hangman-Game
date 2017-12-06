691b635da984a3dbab1e15ae07b4a471df4ca0c0
sflagl <samanthaflagler@gmail.com>
2017-12-06 16:55:51 -0500

    Manipulating the javascript to choose the word random, and know the users keypress

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+// creat an array of words
+
+dancersToGuess = ['Micheal Jackson', 'Usher', 'Justin Timberlake', 'Ciara'];
+
+// Choose word randomly
+
+	var arrNum = Math.floor(Math.random() * dancersToGuess.length);
+	var dancerSelection = dancersToGuess[arrNum]
+	console.log(dancerSelection);
+
+// Create placeholders based on word length
+
+	var dancerHolder = [];
+	for (var i = 0; i < dancerSelection.length; i++) {
+		var createPlaceholder = dancerHolder.push('_');
+
+	}
+	console.log(dancerHolder);
+
+// Get users guess
+	
+	document.addEventListener("keypress", function(event) {
+    
+})
+		
+// check if guess is right
+
+// if right push to right array
+
+	wrongArray = [];
+
+// if wrong push to wrong array
+	
+	rightArray = [];
